0d03ab669e41c8f662597d2a730da9c834d48935
EvgeniyaRo <htstrah@mail.ru>
2018-02-05 23:45:10 +0300

    t

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 	//Мобильная навигация
-	$('.navigation-toggle')on('click', function(e){
+	$('.navigation-toggle').on('click', function(e){
 		e.preventDefault();
-		$('.navigation-list').toggleClass('.navigation-list--open');
+		$('.navigation-list').toggleClass('navigation-list--open');
 	})
 	
 });
